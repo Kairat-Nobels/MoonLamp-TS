48cd459a9f50fdd7e61d5d68d74410316e86ff13
Кайрат <110962052+Kairat-Nobels@users.noreply.github.com>
2023-10-17 15:54:10 +0600

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+import './globals.css'
+
+const poppins = Poppins({ subsets: ['latin'], weight: ["400", "500", "700"], style: ['normal', 'italic'] })
+
+export const metadata: Metadata = {
+  title: 'Moonlamp',
+  description: 'E-commerce website by Kairat',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  )
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'parallax': 'url("../public/parallax.jpg")',
+      },
+      colors: {
+        'primary': '#415a77',
+        'secondary': '#778da9',
+        'dark': '#1b263b',
+        'background': '#e0e1dd',
+      },
+
+    },
+  },
+  plugins: [],
+}
+export default config
